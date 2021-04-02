0491186092e9a0d12b50312d54e7be15cb38b077
manojshimpi <nodemy87@gmail.com>
2021-04-03 02:34:05 +0530

    first commit

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ var express = require('express'),
 app.use('/', express.static(__dirname + '/www'));
 //bind the server to the 80 port
 //server.listen(3000);//for local test
-server.listen(process.env.PORT || 3000);//publish to heroku
+server.listen(process.env.PORT || 3000); //publish to heroku
 //server.listen(process.env.OPENSHIFT_NODEJS_PORT || 3000);//publish to openshift
 //console.log('server started on port'+process.env.PORT || 3000);
 //handle the socket
 io.sockets.on('connection', function(socket) {
     //new user login
     socket.on('login', function(nickname) {
+        console.log("manoj" + nickname);
         if (users.indexOf(nickname) > -1) {
             socket.emit('nickExisted');
         } else {
